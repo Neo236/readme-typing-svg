3a001241008aa581a4f64941e9b67ea945c0471d
Neo236 <LautaroSebastianmambrin@hotmail.com>
2026-07-23 00:56:28 -0300

    fix: autoload por __DIR__ (el require relativo al cwd falla en la lambda)

--- api/index.php
+++ api/index.php
@@ -1,6 +1,12 @@
 <?php declare(strict_types=1);
 
-require "../vendor/autoload.php";
+// resolver el autoload de composer sin depender del cwd
+foreach ([__DIR__ . "/../vendor/autoload.php", "/var/task/vendor/autoload.php", __DIR__ . "/vendor/autoload.php"] as $autoload) {
+    if (file_exists($autoload)) {
+        require $autoload;
+        break;
+    }
+}
 
 // load environment variables if .env exists
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
